c7b23349dd35ec39049458ae37fddd221c70d078
Mohammad Akeel Tariq <16057899@stu.mmu.ac.uk>
2019-03-06 12:49:57 +0000

    updated admin product category input box

--- public/js/main.js
+++ public/js/main.js
@@ -95,7 +95,7 @@ function changeTableButtonText(table)
 //	ADMIN											\\
 //==================================================\\
 $(document).ready(function(){
-	//change product name and price to input boxes
+	//change product name and price to input boxes when clicked on
     $(".productDetails").click(function(e) {
         var input = $("<input>", {
             val: $(this).text(),
@@ -104,12 +104,13 @@ $(document).ready(function(){
         $(this).replaceWith(input);
     });
 
+    //change product name and price to input boxes when button has been pressed
     $("#editProduct").click(function(e){
         e.preventDefault();
         $("span.productDetails").trigger("click");
     });
 
-    //change product category to select box
+    //change product category to select box when clicked on
     $(".productDetails2").click(function(e) {
         var input = $("<select><option>Drinks</option><option>Noodle</option><option>Rice</option><option>Western</option></select>", {
             val: $(this).text(),
@@ -118,8 +119,11 @@ $(document).ready(function(){
         $(this).replaceWith(input);
     });
 
+    //change product category to select box when button has been pressed
     $("#editProduct").click(function(e){
         e.preventDefault();
         $("span.productDetails2").trigger("click");
     });
+
+
 });
