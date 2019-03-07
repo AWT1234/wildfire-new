636aa5a814ac5dbb48573d9f8ebfde6d7af96475
georgiosN <12588783+georgiosN@users.noreply.github.com>
2019-03-07 18:40:05 +0000

    Refactored admin view.

--- public/js/main.js
+++ public/js/main.js
@@ -11,38 +11,3 @@ function changeTableButtonText(tableClicked)
 	tableSelector.innerHTML = "Start/Edit order for Table " + tableClicked;
 	menuTitleNum.innerHTML = "Order for Table " + tableClicked;
 }
-
-//==================================================\\
-//	ADMIN											\\
-//==================================================\\
-$(document).ready(function(){
-	//change product name and price to input boxes when clicked on
-    $(".productDetails").click(function(e) {
-        var input = $("<input>", {
-            val: $(this).text(),
-            type: "<text>" }
-        );
-        $(this).replaceWith(input);
-    });
-
-    //change product name and price to input boxes when button has been pressed
-    $("#editProduct").click(function(e){
-        e.preventDefault();
-        $("span.productDetails").trigger("click");
-    });
-
-    //change product category to select box when clicked on
-    $(".productDetails2").click(function(e) {
-        var input = $("<select><option>Drinks</option><option>Noodle</option><option>Rice</option><option>Western</option></select>", {
-            val: $(this).text(),
-            type: "<text>" }
-        );
-        $(this).replaceWith(input);
-    });
-
-    //change product category to select box when button has been pressed
-    $("#editProduct").click(function(e){
-        e.preventDefault();
-        $("span.productDetails2").trigger("click");
-    });
-});
