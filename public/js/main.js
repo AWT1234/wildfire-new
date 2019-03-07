8aa7cd9be60d6bbefecf9368099feff91455c839
Matt182 <46569070+Matt182@users.noreply.github.com>
2019-03-07 15:30:34 +0000

    Fixed waiter
    fixed waiter passing wrong table name after previous update

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 //==================================================\\
 //	WAITER											\\
 //==================================================\\
+$(document).ready(function()
+{
+    var tableClicked;
+});
+
 function changeTableButtonText(tableClicked)
 {
 	tableSelector.innerHTML = "Start/Edit order for Table " + tableClicked;
