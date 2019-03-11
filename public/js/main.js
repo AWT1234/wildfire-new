7cfce5d729cb2b4cc7d6571936c6b1b03ac00c03
Daniel Lawson <dlawson90@outlook.com>
2019-03-11 13:12:15 +0000

    added print functionality to print view

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,8 @@ function changeTableButtonText(tableClicked)
 	tableSelector.innerHTML = "Start/Edit order for Table " + tableClicked;
 	menuTitleNum.innerHTML = "Order for Table " + tableClicked;
 }
+
+// Print page
+function printFunction() {
+	window.print();
+}
